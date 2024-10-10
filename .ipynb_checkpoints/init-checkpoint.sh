c0991749642bc6511bc8faeb1fea8b37cd779a2c
DeepSyyy <falah.reaggea@gmail.com>
2024-10-10 12:52:27 +0000

    update: hyperparameter tuning

---
+++ .ipynb_checkpoints/init-checkpoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script untuk menginstal nano pada sistem Linux
+
+# Memperbarui repositori paket
+echo "Memperbarui repositori paket..."
+apt-get update
+apt-get install -y nano
+git config --global user.email "falah.reaggea@gmail.com"
+git config --global user.name "DeepSyyy"
+pip install gdown
+pip install pandas
+pip install scikit-learn
+pip install scipy 
+pip install opencv-python
+pip install seaborn
+echo "Inisialisasi Berhasil"
