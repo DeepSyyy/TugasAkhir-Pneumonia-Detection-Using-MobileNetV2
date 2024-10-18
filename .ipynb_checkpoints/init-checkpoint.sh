cb3920aa6416e72248432f8d8fc7f8e2fcf5103c
DeepSyyy <falah.reaggea@gmail.com>
2024-10-18 12:17:24 +0000

    add: model.h5

--- .ipynb_checkpoints/init-checkpoint.sh
+++ .ipynb_checkpoints/init-checkpoint.sh
@@ -1,18 +1,12 @@
-#!/bin/bash
-
-# Script untuk menginstal nano pada sistem Linux
-
-# Memperbarui repositori paket
-echo "Memperbarui repositori paket..."
-apt-get update
-apt-get install -y nano
-git config --global user.email "falah.reaggea@gmail.com"
-git config --global user.name "DeepSyyy"
 pip install gdown
 pip install pandas
 pip install scikit-learn
 pip install scipy 
 pip install opencv-python
 pip install seaborn
-pip install imbalanced-learn
-echo "Inisialisasi Berhasil"
+
+apt update
+apt install nano
+
+git config --global user.email "falah.reaggea@gmail.com"
+git config --global user.name "DeepSyyy"
